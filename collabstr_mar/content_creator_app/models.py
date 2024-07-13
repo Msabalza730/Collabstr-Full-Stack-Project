8fe581003b74b6d2eddfa9ed0e02c97e0565b536
Msabalza730 <maryori_sm730@hotmail.com>
2024-07-13 10:53:40 -0500

    [FEAT] Adding Models

--- collabstr_mar/content_creator_app/models.py
+++ collabstr_mar/content_creator_app/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Creator(models.Model):
+    """Model to save the creator information"""
+
+    name = models.CharField(max_length=100)
+    username = models.CharField(max_length=50)
+    rating = models.FloatField()
+    platform_choices = [
+        ('Instagram', 'Instagram'),
+        ('TikTok', 'TikTok'),
+        ('User Generated Content', 'User Generated Content'),
+    ]
+    platform = models.CharField(max_length=30, choices=platform_choices)
+
+    def __str__(self):
+        return self.username
+
+
+class Content(models.Model):
+    """Model to save the content about creators"""
+    creator = models.ForeignKey(Creator, on_delete=models.CASCADE)
+    url = models.URLField()
+
+    def __str__(self):
+        return self.url
